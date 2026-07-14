9190b05bc3c1df8146b7807d964e35cf2f193682
sparkfreez <abhishek27suvarna@gmail.com>
2026-07-14 22:56:56 +0530

    Added new finger count features

--- fincount/fincount.py
+++ fincount/fincount.py
@@ -1,29 +1,32 @@
 import sys
 import termuxgui as tg
 
-#---voice placeholder---
-def speak_number(count):
-    #In part three we will replace it with android text to speech
-print(f"speaking: {count}")
+camera = "None"
+running = False
 
-last_count = -1
 with tg.Connection() as c:
+
     activity = tg.Activity(c)
 
     layout = tg.LinearLayout(activity, vertical=True)
 
-    title = tg.TextView(activity, "Finger Count", layout)
+    title = tg.TextView(activity, "Finget Counter", layout)
     title.settextsize(24)
 
-    camera_status = tg.Textview(activity, "Cam not Started", layout)
-    camera_status.settextsize(18)
+    status = tg.TextView(activity, "Status Idle", layout)
+    status.settextsize(18)
 
-    finger_display = tg.TextView(activity, "Fingers", layout)
-    finger_display.settextsize(30)
+    camera_label = tg.TextView(activity, "Camera: None", layout)
+    camera_label.settextsize(18)
+
+    preview = tg.TextView(activity, "\n\nCamera Preview\n(Coming in Part 3)\n", layout)
+    preview.settextsize(20)
+
+    finger_count = tg.TextView(activity, "Fingers: 0", layout)
+    finger_count.settextsize(30)
 
     front_btn = tg.Button(activity, "Front Camera", layout)
-    rear_btn = tg.Button(activity, "Rear Camera", layout)
-    start_btn = tg.Button(activity, "Start Detection", layout)
-    stop_btn = tg.Button(activity, "Stop Detection", layout)
-    exit_btn = tg.Button(activity, "Exit", layout)
-
+    rear_btn = tg.Button(activity, "Rear Camera"          , layout)
+    start_btn = tg.Button(activity, "Start Camera"          , layout)
+    stop_btn = tg.Button(activity, "Stop Camera"          , layout)
+    exit_btn = tg.Button(activity, "Exit"                    , layout)
